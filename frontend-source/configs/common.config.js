4dbfe945f37dc5c478e6edbe26cc38609689bff3
ayoah <ayoahy@gmail.com>
2016-10-20 13:52:03 +0800

    refs 1.整合webpack+express 2.利用webpack-dev-middleware和webpack-hot-middleware实现热替换 3.使用webpack的imports-loader组件为配置文件注入环境变量

---
+++ frontend-source/configs/common.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    author: 'fun',
+    area  : "wuxi",
+    env   : webpack_evn == 'dev' ? '开发环境' : '非开发环境[' + webpack_evn + ']'
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,4 @@
-module.exports = {
-    entry: {
-        page1: ['./client/page1', hotMiddlewareScript],
-        page2: ['./client/page2', hotMiddlewareScript]
-    },
-    output: {
-        filename: './[name]/bundle.js',
-        path: path.resolve('./public'),
-        publicPath: publicPath
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [{
-            test: /\.(png|jpg)$/,
-            loader: 'url?limit=8192&context=client&name=[path][name].[ext]'
-        }, {
-            test: /\.scss$/,
-            loader: 'style!css?sourceMap!resolve-url!sass?sourceMap'
-        }]
-    },
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
-    ]
-};
+module.exports = require('./webpack.module.js')('test');
+
+
+
